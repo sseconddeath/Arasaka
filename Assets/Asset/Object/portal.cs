cbcda7286e02ad3e74dea5e38ff2e7adefe8dfa6
Daniil <daniil.kuzru@mail.ru>
2024-10-16 22:31:12 +0500

    light stage 2

---
+++ Assets/Asset/Object/portal.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portal : MonoBehaviour
+{
+    public Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
